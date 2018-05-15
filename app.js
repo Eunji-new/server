df2c39b29d02cc27e788cc4cfd27d2b5a4ec40e4
dmswl9898 <sejj0126@naver.com>
2018-05-15 19:04:43 +0900

    sejsejjj

--- app.js
+++ app.js
@@ -9,10 +9,25 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , bodyPaser = require('body-parser')
+var mysql= require('mysql');
 var app = express();
+var client = mysql.createConnection({
+	hostname:"127.0.0.1:3306", 
+	user : "root",
+	password:"1522653as",
+	database:"darack"
+});
+
+client.connect(function(err){
+	console.log('MysqlCONNection');
+	if(err){console.error(err);
+	throw err;
+}
+});
+var session=require('express-session');
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3004);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
@@ -22,7 +37,11 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(session({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized:true
+}));
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
@@ -32,15 +51,54 @@ app.get('/', function(req, res){
 	res.render('index.html');
 });
 app.get('/signup', function(req, res){
-	res.render('signup.html');
+	res.render('SignUp.html');
 });
 
 app.post('/logincheck', function(req, res){
-	if(req.body.id=="test"){
-		if(req.body.pw=="test"){
-			res.send("<h1>WELCOME</h1>");
-		}
-	}
+	var uid=req.body.id;
+	var upw=req.body.pw;
+	var connection = client.query('SELECT count(*) cnt from USER where user_id=? and user_pw=?',[uid,upw],function(err, rows){
+		if(err){
+			console.error('err',err);
+			}
+		
+		 var cnt = rows[0].cnt;
+	      
+	      if( cnt === 1 ){
+	    	//  req.session.user_id=uid;
+	         res.send( '<h1>login success</h1>');
+	      
+	      }else{
+	         res.json({result:'fail'});
+	         res.send('<script> alert("id or passwd is wrong"); history.back();</script>');
+	      }
+	});
+});
+
+app.post('/signcheck', function(req,res){
+	var nid=req.body.nid;
+	var uid=req.body.id;
+	var upw=req.body.pw;
+	var upn=req.body.pn;
+	var uemail = req.body.email;
+	var ubirth=req.body.birth;
+	var uname = req.body.name;
+	
+	var connection = client.query('INSERT into USER (user_id, user_email, user_pw, user_phonenumber, user_birth, user_name) values(?,?,?,?,?,?)',[uid,uemail,upw,upn,ubirth,uname],function(err, rows){
+		
+		console.log(rows);
+		
+		if(err){
+				console.error('err',err);
+				
+				throw err;
+				
+				}
+			
+			console.log(connection);
+		});
+	
+	res.send('<h1>success</h1>');
 });
 
 
